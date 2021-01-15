03a75670a9e8a4ae9edf0ab2ac87b32d02c63e11
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-15 16:56:42 +0000

    test

--- client.js
+++ client.js
@@ -7,9 +7,29 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  process.stdin.setEncoding('utf-8');
   conn.on('data', (data) => {
-		console.log('Server says: ', data);
-		});
+    console.log('Server says: ', data);
+    });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: BBH');   
+  });
+  conn.on('connect', () => {
+    conn.write( "Move: up");   
+  });
+  conn.on('connect', () => {
+    conn.write( "Move: down");   
+  });
+  conn.on('connect', () => {
+    conn.write( "Move: left");   
+  });
+  conn.on('connect', () => {
+    conn.write( "Move: right");   
+  });
+ 
+ 
   return conn;
 }
 module.exports = connect;
+
