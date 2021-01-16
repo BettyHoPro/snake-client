88233b7fd87e841adb9419e70fbb575d18b43e2b
BettyHoPro <bettyvisualtech@gmail.com>
2021-01-16 06:33:35 +0000

    Base state where snake shows up, but does not move

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+
 const connect = function() {
   const conn = net.createConnection({ 
     // host: '10.0.2.15',
@@ -8,28 +9,15 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   process.stdin.setEncoding('utf-8');
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
-    });
+  });
+  
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    conn.write('Name: BBH');   
-  });
-  conn.on('connect', () => {
-    conn.write( "Move: up");   
-  });
-  conn.on('connect', () => {
-    conn.write( "Move: down");   
-  });
-  conn.on('connect', () => {
-    conn.write( "Move: left");   
-  });
-  conn.on('connect', () => {
-    conn.write( "Move: right");   
-  });
- 
- 
+    conn.write('Name: BBH');
+  })
   return conn;
 }
 module.exports = connect;
-
